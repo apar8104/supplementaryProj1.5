bb682dd6b5a4f8d41de4ca8b27234ae3638c5162
apar8104 <apar8104@students.vsu.edu>
2025-02-12 17:39:18 -0500

    test: added test for cube side value being negative

--- AbsShape3D.Tests/CubeTests.cs
+++ AbsShape3D.Tests/CubeTests.cs
@@ -3,9 +3,15 @@ namespace AbsShape3D.Tests
     public class CubeTests
     {
         [Fact]
-        public void Contructor_ShouldThrowException_WhenSideIsZero()
+        public void Constructor_ShouldThrowException_WhenSideIsZero()
         {
             Assert.Throws<ArgumentException>(() => new Cube(0));
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowException_WhenSideIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() => new Cube(-5));
         }
     } 
 }
