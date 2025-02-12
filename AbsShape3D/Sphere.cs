41eaad7a9bb3565f7c0337f69a120d2814fef30e
apar8104 <apar8104@students.vsu.edu>
2025-02-12 18:11:11 -0500

    chore: created sphere class

---
+++ AbsShape3D/Sphere.cs
@@ -0,0 +1,34 @@
+using System.Net.Http.Headers;
+
+namespace AbsShape3D
+{
+    public class Sphere : AbsShape3D
+    {
+        private double radius;
+
+        public Sphere(double radius)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Validate (double radius)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetVolume()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetSurfaceArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Dump()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
