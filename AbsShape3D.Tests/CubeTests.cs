3ea38d44aeb493c7c3298598f3aabc703e0c25f1
apar8104 <apar8104@students.vsu.edu>
2025-02-12 17:41:42 -0500

    test: added test for cube getvolume

--- AbsShape3D.Tests/CubeTests.cs
+++ AbsShape3D.Tests/CubeTests.cs
@@ -13,5 +13,16 @@ namespace AbsShape3D.Tests
         {
             Assert.Throws<ArgumentException>(() => new Cube(-5));
         }
+
+        [Fact]
+        public void GetVolume_ShouldReturnCorrectValue()
+        {
+            double side = 3;
+            Cube cube = new Cube(side);
+
+            double volume = cube.GetVolume();
+
+            Assert.Equal(27, volume);
+        }
     } 
 }
