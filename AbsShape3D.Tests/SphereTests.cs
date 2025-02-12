c5bbf1f7b746d47526817a61446cc45880719048
apar8104 <apar8104@students.vsu.edu>
2025-02-12 18:14:02 -0500

    test: added test for sphere validate radius is not zero

--- AbsShape3D.Tests/SphereTests.cs
+++ AbsShape3D.Tests/SphereTests.cs
@@ -1,4 +1,11 @@
 namespace AbsShape3D.Tests
 {
-    
+    public class SphereTests
+    {
+        [Fact]
+        public void Constructor_ShouldThrowException_WhenRadiusIsZero()
+        {
+            Assert.Throws<ArgumentException>(() => new Sphere(0));
+        }
+    }
 }
