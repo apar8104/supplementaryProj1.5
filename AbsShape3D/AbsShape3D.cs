8273030ff35020673934f158db1300a21de40531
apar8104 <apar8104@students.vsu.edu>
2025-02-12 17:05:49 -0500

    doc: added documentation for GetVolume class

--- AbsShape3D/AbsShape3D.cs
+++ AbsShape3D/AbsShape3D.cs
@@ -2,6 +2,10 @@
 
 public abstract class AbsShape3D
 {
+    /// <summary>
+    /// Calculates the volume of a shape. 
+    /// </summary>
+    /// <returns>The volume of the shape as a double. </returns>
     public abstract double GetVolume();
 
     public abstract double GetSurfaceArea();
