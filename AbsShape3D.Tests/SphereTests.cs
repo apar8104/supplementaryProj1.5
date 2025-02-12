b8d6f6cb3da4b8fc52d7510777530d7008e4a504
apar8104 <apar8104@students.vsu.edu>
2025-02-12 18:15:26 -0500

    test: added test for sphere validate radius is not negative

--- AbsShape3D.Tests/SphereTests.cs
+++ AbsShape3D.Tests/SphereTests.cs
@@ -7,5 +7,11 @@ namespace AbsShape3D.Tests
         {
             Assert.Throws<ArgumentException>(() => new Sphere(0));
         }
+
+        [Fact]
+        public void Constructor_ShouldThrowException_WhenRadiusIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() => new Sphere(-5));
+        }
     }
 }
