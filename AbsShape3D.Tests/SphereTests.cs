4c050bf480ac7e43d8430b478376857df4a92429
apar8104 <apar8104@students.vsu.edu>
2025-02-12 18:18:23 -0500

    test: added test for sphere getvolume

--- AbsShape3D.Tests/SphereTests.cs
+++ AbsShape3D.Tests/SphereTests.cs
@@ -13,5 +13,16 @@ namespace AbsShape3D.Tests
         {
             Assert.Throws<ArgumentException>(() => new Sphere(-5));
         }
+
+        [Fact]
+        public void GetVolume_ShouldReturnCorrectVolume()
+        {
+            double radius = 3;
+            Sphere sphere = new Sphere(radius);
+
+            double volume = sphere.GetVolume();
+
+            Assert.Equal((4.0/3.0) * Math.PI * Math.Pow(radius, 3), volume, 5);
+        }
     }
 }
