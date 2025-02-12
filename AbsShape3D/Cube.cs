0f032a9e3ca1555949f3076bb9caa1dde8dcd3c6
apar8104 <apar8104@students.vsu.edu>
2025-02-12 17:33:48 -0500

    chore: created cube class

---
+++ AbsShape3D/Cube.cs
@@ -0,0 +1,34 @@
+using System.Net.Http.Headers;
+
+namespace AbsShape3D{
+    public class Cube : AbsShape3D
+    {
+        private double side;
+
+        public Cube(double side)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Validate(double side)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetVolume()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetSurfaceArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Dump()
+        {
+            throw new NotImplementedException();
+            
+        }
+    }
+}
