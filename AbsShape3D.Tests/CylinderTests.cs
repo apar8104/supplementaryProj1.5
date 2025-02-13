fc008095987568216586b439955f6c6221f363bf
apar8104 <apar8104@students.vsu.edu>
2025-02-12 23:25:26 -0500

    test: added test for cylinder validate radius is not zero

--- AbsShape3D.Tests/CylinderTests.cs
+++ AbsShape3D.Tests/CylinderTests.cs
@@ -1,4 +1,11 @@
 namespace AbsShape3D.Tests
 {
-    
+    public class CylinderTests
+    {
+        [Fact]
+        public void Constructor_ShouldThrowException_WhenRadiusIsZero()
+        {
+            Assert.Throws<ArgumentException>(() => new Cylinder(0, 5));
+        }
+    }
 }
