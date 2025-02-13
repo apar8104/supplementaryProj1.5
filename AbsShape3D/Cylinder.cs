f5c1ae03a7a00c7c9009dcea44528091572c827c
apar8104 <apar8104@students.vsu.edu>
2025-02-12 23:45:22 -0500

    doc: added documentation for cylinder creation function

--- AbsShape3D/Cylinder.cs
+++ AbsShape3D/Cylinder.cs
@@ -5,6 +5,11 @@ namespace AbsShape3D
         private double radius;
         private double height;
 
+        /// <summary>
+        /// Initializes a new instance of the cylinder class. 
+        /// </summary>
+        /// <param name="radius">The radius of the cylinder. </param>
+        /// <param name="height">The height of the cylinder. </param>
         public Cylinder(double radius, double height)
         {
             Validate(radius, height);
