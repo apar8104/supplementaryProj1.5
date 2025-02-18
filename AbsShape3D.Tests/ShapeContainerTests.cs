2f7cd8949a1912e4ad59496d1d6b5c0bf23d06da
apar8104 <apar8104@students.vsu.edu>
2025-02-18 16:18:43 -0500

    test: added Create should add shape to container to ShapeContainerTests

--- AbsShape3D.Tests/ShapeContainerTests.cs
+++ AbsShape3D.Tests/ShapeContainerTests.cs
@@ -1,4 +1,16 @@
 namespace AbsShape3D.Tests
 {
-    
+    public class ShapeContainerTests
+    {
+        [Fact]
+        public void Create_ShouldAddShapeToContainer()
+        {
+            var container = new ShapeContainer();
+            var cube = new Cube(2);
+
+            container.Create(cube);
+
+            Assert.Equal(cube, container.Get(0));
+        }
+    }
 }
