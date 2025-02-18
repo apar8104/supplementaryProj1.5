f4ed1e64477f34b9a4f46ecd5c0d81a99e4144ea
apar8104 <apar8104@students.vsu.edu>
2025-02-18 16:21:20 -0500

    test: added Create should return correct shape to ShapeContainerTests

--- AbsShape3D.Tests/ShapeContainerTests.cs
+++ AbsShape3D.Tests/ShapeContainerTests.cs
@@ -12,5 +12,18 @@ namespace AbsShape3D.Tests
 
             Assert.Equal(cube, container.Get(0));
         }
+
+        [Fact]
+        public void Create_ShouldReturnCorrectShape()
+        {
+            var container = new ShapeContainer();
+            var sphere = new Sphere(3);
+
+            container.Create(sphere);
+
+            var result = container.Get(0);
+
+            Assert.Equal(sphere, result);
+        }
     }
 }
